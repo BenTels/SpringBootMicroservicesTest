6185d9765b71064a06cce6ca2db161e941404192
Benjamin Ze'ev Tels <bzt@bentels.nl>
2021-06-28 00:03:43 +0200

    Added CORS

--- spring-security-test/src/main/java/nl/bentels/test/sec/SecurityTestApplication.java
+++ spring-security-test/src/main/java/nl/bentels/test/sec/SecurityTestApplication.java
@@ -2,6 +2,9 @@ package nl.bentels.test.sec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SecurityTestApplication {
@@ -10,4 +13,13 @@ public class SecurityTestApplication {
 		SpringApplication.run(SecurityTestApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("GET", "POST", "DELETE").allowedOrigins("*");
+			}
+		};
+	}
 }
